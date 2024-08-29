66b2ab5eefa2ad6f875a66069e3cd2b918bb67cb
ctice4 <ctice4@huskers.unl.edu>
2024-08-29 10:09:26 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Carson Tice
+ * Date: 2024/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Carson Tice\n");
+
+    return 0;
+}
+
